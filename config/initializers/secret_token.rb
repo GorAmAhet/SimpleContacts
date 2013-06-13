543f4365310fecdbcc1e38e79c6a7102e2ec5de1
oleg <oleg@nn.ru>
2013-06-13 14:48:54 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SimpleContacts::Application.config.secret_token = '343151681c47672d63e3ad1707192911b5ebf17780ebd95f19c7a78105e78a7459a09249f7504967db2e1c02f70b137d0bd4f737bf091a83ccdedb2581a6b70d'
